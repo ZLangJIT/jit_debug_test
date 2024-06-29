5d634cdd69f1e45da0bc4da287b948553ff2add4
root <root@localhost.localdomain>
2024-06-29 17:38:58 +0000

    try to build

--- try_get_release.sh
+++ try_get_release.sh
@@ -8,7 +8,7 @@ TAG=$3
 URL=https://github.com/$1/$2/releases/download/$TAG/$FILE
 R=$(curl --silent -I $URL | grep -E "^HTTP" | awk -F " " '{print $2}')
 RSHA=$(curl --silent -I $URL.sha512 | grep -E "^HTTP" | awk -F " " '{print $2}')
-if [[ ("$R" = "200" || "$R" = "302") && ("$RSHA" == "200" || "$RSHA" == "302") ]]
+if [[ ("$R" == "200" || "$R" == "302") && ("$RSHA" == "200" || "$RSHA" == "302") ]]
     then
         echo "file '$FILE' exists in releases"
         curl -L $URL.sha512 -o $FILE.sha512
